f7ef6290a90abbf50caf15018cca011991379249
kclaireyh <kimu.yohyon@gmail.com>
2021-11-10 23:48:11 +0900

    add: tap navigation icon

--- pages/navigations/tabNavigator.tsx
+++ pages/navigations/tabNavigator.tsx
@@ -1,28 +1,49 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import HomeNavigator from './homeNavigator';
 import MyPageNavigator from './mypageNavigator';
 import SearchNavigator from './searchNavigator';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const TabNavigator = () => {
-  const Tab = createBottomTabNavigator();
+  const Tab = createMaterialBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Search" component={SearchNavigator}></Tab.Screen>
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="black"
+      inactiveColor="#a0a0a0"
+      barStyle={{ backgroundColor: '#FFD600' }}>
+      <Tab.Screen
+        name="Search"
+        component={SearchNavigator}
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="search" color={color} size={26} />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Home"
         component={HomeNavigator}
         options={{
-          headerShown: false,
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" color={color} size={26} />
+          ),
         }}
       />
       <Tab.Screen
         name="MyPage"
         component={MyPageNavigator}
         options={{
-          headerShown: false,
-        }}></Tab.Screen>
+          tabBarLabel: 'MyPage',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" color={color} size={26} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
