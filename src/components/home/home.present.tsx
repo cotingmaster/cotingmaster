5b48d06dbd3bba93d26231f53b4653a7fba5c747
geunangel <1004geuna1004@gmail.com>
2021-11-11 19:28:56 +0900

    김근아 add : Main CSS, FreeBoard CSS수정

--- src/components/home/home.present.tsx
+++ src/components/home/home.present.tsx
@@ -1,13 +1,56 @@
 import React from 'react';
-import { Wrapper, MainText, Button1 } from './home.styles';
+import {
+  LogoImg,
+  LogoIcon,
+  Wrapper,
+  MainText,
+  SharingInfo,
+  Meeting,
+  FreeBoard,
+  SubText,
+  MainBox,
+  MainIcon,
+} from './home.styles';
 import { useNavigation } from '@react-navigation/native';
 
-const HomeUI = ({ navigate }) => {
+const HomeUI = ({ navigate }: any) => {
   const navigation = useNavigation();
   return (
     <Wrapper>
-      <MainText>여기는 메인페이지입니다</MainText>
-      <Button1
+      {/* <TopView>
+        <MainText>여기는 메인페이지입니다</MainText>
+      </TopView> */}
+      <LogoImg>
+        <LogoIcon source={require('../../../public/images/coting.png')} />
+      </LogoImg>
+      <SharingInfo onPress={() => navigation.navigate('정보공유게시판')}>
+        <MainBox>
+          <MainText>정보공유</MainText>
+          <SubText>취업정보, IT관련 ...</SubText>
+        </MainBox>
+        <MainIcon source={require('../../../public/images/arrow.png')} />
+      </SharingInfo>
+      <Meeting onPress={() => navigation.navigate('만남게시판')}>
+        <MainBox>
+          <MainText>만남</MainText>
+          <SubText>만날사람모여라~</SubText>
+        </MainBox>
+        <MainIcon source={require('../../../public/images/arrow.png')} />
+      </Meeting>
+      <FreeBoard onPress={() => navigation.navigate('커뮤니티게시판')}>
+        <MainBox>
+          <MainText>커뮤니티</MainText>
+          <SubText>자유게시물, 일상 ...</SubText>
+        </MainBox>
+        <MainIcon source={require('../../../public/images/arrow.png')} />
+      </FreeBoard>
+    </Wrapper>
+  );
+};
+export default HomeUI;
+
+{
+  /* <Button1
         title="정보공유게시판가기"
         onPress={() => navigation.navigate('정보공유게시판')}
       />
@@ -18,9 +61,5 @@ const HomeUI = ({ navigate }) => {
       <Button1
         title="커뮤니티게시판가기"
         onPress={() => navigation.navigate('커뮤니티게시판')}
-      />
-    </Wrapper>
-  );
-};
-
-export default HomeUI;
+      /> */
+}
