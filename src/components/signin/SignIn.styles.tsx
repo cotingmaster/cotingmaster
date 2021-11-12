76960da2e5b4e86b4e623b67eb0c3cf8c69fc518
kclaireyh <kimu.yohyon@gmail.com>
2021-11-12 19:08:24 +0900

    fix: login css

--- src/components/signin/SignIn.styles.tsx
+++ src/components/signin/SignIn.styles.tsx
@@ -5,26 +5,38 @@ export const S = {
     flex: 1;
     padding: 10px;
   `,
-  LineBox: styled.View`
-    flex: 1;
+  LogoBackground: styled.View`
+    margin-top: 50px;
+    height: 90px;
+    width: 100%;
+    background-color: black;
+    z-index: -1;
     display: flex;
-    flex-direction: row;
-    margin-top: 10px;
+    align-items: center;
+    justify-content: center;
   `,
+  Logo: styled.Image`
+    margin: auto;
+  `,
+
   InputLine: styled.TextInput`
     height: 50px;
-    width: 400px;
+    width: 100%;
+    margin-top: 30px;
     margin-right: 10px;
-    border-bottom-color: gray;
-    border-bottom-width: 1px;
+    border-radius: 10px;
+    border-color: gray;
+    border-radius: 10px;
+    border-width: 1px;
+    padding-left: 10px;
+    font-size: 18px;
   `,
   ShowText: styled.Text`
     /* height: 30px; */
     font-size: 18px;
   `,
-  SubmitButton: styled.Button`
+  PressSubmit: styled.TouchableOpacity`
     margin: 10px;
-    flex: 1;
     height: 30px;
     font-size: 20px;
     color: aliceblue;
--- src/components/signin/signup/SignUp.container.tsx
+++ src/components/signin/signup/SignUp.container.tsx
@@ -1,9 +1,58 @@
 import * as React from 'react';
-import SignUpUI from "./SignUp.presenter"
+import SignUpUI from './SignUp.presenter';
+import { useMutation } from '@apollo/client';
+import { useState } from 'react';
+import { CREATE_USER } from './SignUp.queries';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function SingUp () {
+export default function SingUp() {
+  // const [signup, setSignup] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const [createUser] = useMutation(CREATE_USER);
+  const navigation = useNavigation();
+
+  const changeEmail = (e: any) => {
+    setEmail(e);
+  };
+
+  const changePassword = (e: any) => {
+    setPassword(e);
+  };
+
+  const changeName = (e: any) => {
+    setName(e);
+  };
+
+  const onPressSubmit = async () => {
+    const variables = {
+      createUserInput: {
+        email,
+        password,
+        name,
+      },
+    };
+    try {
+      const { data } = await createUser({ variables });
+      console.log(data?.createUser._id);
+      Alert.alert(
+        `${data?.createUser.name}님 가입성공! 가입하신 정보로 로그인을 해주세요`,
+      );
+      navigation.navigate('로그인페이지');
+    } catch (error: any) {
+      Alert.alert(error.message);
+    }
+  };
 
   return (
-    <SignUpUI />
-  )
+    <SignUpUI
+      changeEmail={changeEmail}
+      changePassword={changePassword}
+      changeName={changeName}
+      onPressSubmit={onPressSubmit}
+    />
+  );
 }
