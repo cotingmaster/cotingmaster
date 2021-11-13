45feb3a587db3c6207d2ab147e1efaafbf607473
kclaireyh <kimu.yohyon@gmail.com>
2021-11-13 19:25:50 +0900

    fix: MypageMain username표시, 로그아웃 기능구현

--- src/components/mypageMain/MypageMain.container.tsx
+++ src/components/mypageMain/MypageMain.container.tsx
@@ -1,12 +1,33 @@
-import { useQuery } from '@apollo/client';
-import React from 'react';
+import { useMutation, useQuery } from '@apollo/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useContext } from 'react';
+import { Alert } from 'react-native';
 import MyPageMainUI from './MypageMain.present';
 import { FETCH_USER_LOGGEDIN } from './MypageMain.queries';
+import { GlobalContext } from '../../../App';
 
 const MyPageMainContainer = () => {
   const { data } = useQuery(FETCH_USER_LOGGEDIN);
+  const { setUserInfo, setAccessToken } = useContext(GlobalContext);
+  // const [logoutUser] = useMutation(LOGOUT_USER);
 
-  return <MyPageMainUI data={data} />;
+  const onPressLogout = () => {
+    // try {
+    //   logoutUser();
+    // } catch (e: any) {
+    //   Alert.alert(e.message);
+    // }
+    try {
+      AsyncStorage.removeItem('user');
+      setUserInfo({});
+      setAccessToken('');
+      Alert.alert('로그아웃 성공');
+    } catch (error) {
+      Alert.alert('error.message');
+    }
+  };
+
+  return <MyPageMainUI data={data} onPressLogout={onPressLogout} />;
 };
 
 export default MyPageMainContainer;
