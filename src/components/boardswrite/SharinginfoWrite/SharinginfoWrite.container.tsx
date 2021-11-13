f8c85f307513c3cd56ed6b8e1eb69e5d73cb8ea9
geunangel <1004geuna1004@gmail.com>
2021-11-13 19:24:49 +0900

    김근아 add : SharingBoard, FreeBoard 페이지 등록  구현. firebase DB 연동

--- src/components/boardswrite/SharinginfoWrite/SharinginfoWrite.container.tsx
+++ src/components/boardswrite/SharinginfoWrite/SharinginfoWrite.container.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SharingInfoWriteUI from './SharinginfoWrite.present';
 import { useNavigation } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
+import { useQuery } from '@apollo/client';
+import { FETCH_USER_LOGGEDIN } from '../../signin/SignIn.queries';
 
 const SharingInfoWriteContainer = () => {
   const navigation = useNavigation();
+  const { data } = useQuery(FETCH_USER_LOGGEDIN);
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
 
-  const onClickSubmit = () => {
+  const onPressSharingSubmit = () => {
+    const user_id = data?.fetchUserLoggedIn._id;
+    console.log(data?.fetchUserLoggedIn._id);
+
+    firestore()
+      .collection('Users')
+      .doc(data?.fetchUserLoggedIn._id)
+      .collection('SharingBoard')
+      .doc()
+      .set({
+        title,
+        contents,
+        createdAt: new Date(),
+        tag: 'SharingBoard',
+      });
     navigation.navigate('정보공유게시판디테일');
   };
 
-  return <SharingInfoWriteUI onClickSubmit={onClickSubmit} />;
+  return (
+    <SharingInfoWriteUI
+      onPressSharingSubmit={onPressSharingSubmit}
+      setTitle={setTitle}
+      setContents={setContents}
+    />
+  );
 };
 
 export default SharingInfoWriteContainer;
